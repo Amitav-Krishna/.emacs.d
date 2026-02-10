6c0a5171e0251949b6080113cfee614e50e30b99
Amitav Krishna <amtiavkrishna2011@gmail.com>
2026-02-10 17:23:29 -0500

    Stuff

--- init.el
+++ init.el
@@ -1,5 +1,14 @@
 ;;; init.el --- Minimal Emacs config -*- lexical-binding: t; -*-
 
+;; Suppress native-comp warnings from third-party packages
+(setq native-comp-async-report-warnings-errors 'silent)
+
+;; Ensure ~/.deno/bin and ~/.local/bin are in exec-path
+(dolist (dir '("~/.deno/bin" "~/.local/bin"))
+  (let ((expanded (expand-file-name dir)))
+    (add-to-list 'exec-path expanded)
+    (setenv "PATH" (concat expanded ":" (getenv "PATH")))))
+
 ;; Package setup
 (require 'package)
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
@@ -90,74 +99,19 @@
 (use-package magit
   :bind ("C-x g" . magit-status))
 
-;;; init.el ends here
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(org-roam-ui magit vterm lsp-pyright lsp-ui lsp-mode org-roam)))
->>>>>>> fc6b599 (First commit from new device)
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-<<<<<<< HEAD
-(setq-default truncate-lines nil)
-(define-key org-mode-map (kbd "C-u C-c C-l") 'org-toggle-link-display)
-(flyspell-mode 1)
-(setq org-confirm-babel-evaluate nil)
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(org-agenda-files '("~/notes/calendar.org"))
- '(package-selected-packages
-   '(code-cells company counsel ein flycheck gptel htmlize ivy-bibtex
-		jupyter latex-table-wizard lsp-treemacs lsp-ui magit
-		org-noter org-roam-ui pdf-tools projectile subed vterm)))
-
-(global-set-key (kbd "C-c b") 'ivy-bibtex)
-
-
-
-;; Packages
-(use-package deferred :ensure t)
-(use-package request :ensure t)
-(use-package ein
-  :ensure t)
-(use-package polymode
-  :ensure t)
-(use-package jupyter
-  :ensure t)
-(require 'ob-python)
-(require 'ob-jupyter)
-(use-package lsp-mode
+;; Ivy/Counsel/Swiper (required for ivy-bibtex)
+(use-package ivy
   :ensure t
-  :hook ((c++-mode . lsp)
-         (c-mode . lsp))
-  :commands lsp)
-(use-package company
-  :ensure t
-  :hook (after-init . global-company-mode))
-(use-package flycheck
-  :ensure t
-  :init (global-flycheck-mode))
-(use-package lsp-ui
-  :ensure t
-  :commands lsp-ui-mode
-  :hook (lsp-mode . lsp-ui-mode))
-(use-package lsp-treemacs
-  :ensure t
-  :after lsp)
-(use-package pdf-tools
-  :ensure t
-  :config (pdf-tools-install))
+  :diminish
+  :bind (("C-s" . swiper)
+         ("M-x" . counsel-M-x)
+         ("C-x C-f" . counsel-find-file)
+         ("C-x b" . ivy-switch-buffer))
+  :config
+  (ivy-mode 1)
+  (setq ivy-use-virtual-buffers t
+        ivy-count-format "(%d/%d) "
+        enable-recursive-minibuffers t))
 
 (use-package counsel
   :ensure t
@@ -168,50 +122,100 @@
   :ensure t
   :after ivy)
 
-(use-package ivy
-  :ensure t
-  :diminish
-  :bind (("C-s" . swiper)               ;; search in buffer
-         ("M-x" . counsel-M-x)          ;; better M-x
-         ("C-x C-f" . counsel-find-file) ;; better file finder
-         ("C-x b" . ivy-switch-buffer))
-  :config
-  (ivy-mode 1)
-  (setq ivy-use-virtual-buffers t
-        ivy-count-format "(%d/%d) "
-        enable-recursive-minibuffers t))
+;; Additional settings from old config
+(setq-default truncate-lines nil)
+(with-eval-after-load 'org
+  (define-key org-mode-map (kbd "C-u C-c C-l") 'org-toggle-link-display))
+(flyspell-mode 1)
+(setq org-confirm-babel-evaluate nil)
+
+;; Slideshow export - only export content tagged with :slides:
+(setq org-export-select-tags '("slides"))
+
+;; BibTeX configuration
 (use-package ivy-bibtex
   :ensure t
   :after ivy
   :config
-  ;; Set the path to your .bib file
   (setq bibtex-completion-bibliography '("~/books/references.bib")
         bibtex-completion-library-path '("~/books/")
         bibtex-completion-notes-path "~/org/roam/notes/"
         bibtex-completion-pdf-field "file")
-  ;; Optional: use ivy for completion
   (setq ivy-bibtex-default-action 'ivy-bibtex-open-any))
 
+(global-set-key (kbd "C-c b") 'ivy-bibtex)
 
+;; Org Babel languages
+(with-eval-after-load 'org
+  (org-babel-do-load-languages
+   'org-babel-load-languages
+   '((python . t)
+     (java . t))))
 
-(unless (package-installed-p 'org-roam)
-  (package-refresh-contents)
-  (package-install 'org-roam))
-(use-package org-roam
+;; Default Java blocks to tangle with auto-generated filenames
+;; Use :tangle yes on source blocks, files will be named <block-name>.java
+;; To include org filename, use explicit :tangle <filename>-<block>.java
+
+;; C/C++ LSP (already loaded for Python above, just add hooks)
+(add-hook 'c++-mode-hook #'lsp)
+(add-hook 'c-mode-hook #'lsp)
+
+;; Company mode for completions
+(use-package company
   :ensure t
-  :custom
-  (org-roam-directory "~/org/roam/notes/")
-  :bind (("C-c n l" . org-roam-buffer-toggle)
-         ("C-c n f" . org-roam-node-find)
-         ("C-c n i" . org-roam-node-insert))
+  :hook (after-init . global-company-mode))
+
+;; Flycheck
+(use-package flycheck
+  :ensure t
+  :init (global-flycheck-mode))
+
+;; Treemacs
+(use-package lsp-treemacs
+  :ensure t
+  :after lsp)
+
+;; PDF Tools
+(use-package pdf-tools
+  :ensure t
+  :config (pdf-tools-install))
+
+;; --- Org-reveal (slideshows) ---
+(use-package ox-reveal
+  :ensure t
+  :demand t
+  :after org
   :config
-  (org-roam-setup))
-(use-package gptel)
-(load (expand-file-name "secrets.el" user-emacs-directory) t)
-(gptel-make-deepseek "DeepSeek"       ;Any name you want
-  :stream t                           ;for streaming responses
-  :key deepseek-api-key)               ;can be a function that returns the key
-;;; actual API key is in secrets.el which is in the .giignore so that I don't get pwned
+  (require 'ox-reveal)
+  :custom
+  (org-reveal-root "https://cdn.jsdelivr.net/npm/reveal.js@4.5.0")
+  (org-reveal-mathjax t))
+
+;; --- empv (mpv media player) ---
+(use-package empv
+  :ensure t
+  :custom (empv-invidious-instance 'ivjs)
+  :bind-keymap ("C-c m" . empv-map))
+
+;; --- Telega (Telegram client) ---
+(use-package telega
+  :commands telega)
+
+(require 'exwm)
+(setq exwm-input-global-keys `(([?\s-r] . exwm-reset)))
+(exwm-wm-mode)
 ;;; init.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(telega empv popup synosaurus exwm ox-reveal code-cells company counsel flycheck htmlize ivy-bibtex lsp-pyright lsp-treemacs lsp-ui magit marginalia orderless org-roam pdf-tools subed swiper vertico vterm)))
 
-
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
